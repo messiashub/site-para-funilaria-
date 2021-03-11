383c8793ef01569143eb5f54f660f9957ddbd2c9
messiashub <claudineimessias@hotmail.com>
2021-03-11 01:14:26 -0300

    update

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const toNav = nav.offsetTop;
 console.log(nav.classList)
 
 
-window.onscroll = ()=>{
+window.onscroll = () => {
     ficarNoTop();
 }
 
 
-function ficarNoTop(){
-    if(window.pageYOffset >= toNav){
+function ficarNoTop() {
+    if (window.pageYOffset >= toNav) {
         nav.classList.add("fixed-top")
-    }else{
+    } else {
         nav.classList.remove("fixed-top")
     }
 }
@@ -21,12 +21,12 @@ const elementos = document.querySelectorAll('[data-anima]');
 console.log(elementos)
 const animacaoClass = "animacao";
 
-function animaScroll(){
-    const topoPagina = window.pageYOffset+((window.innerHeight * 3)/4);
-    elementos.forEach(function(elemento){
-        if(topoPagina > elemento.offsetTop){
+function animaScroll() {
+    const topoPagina = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    elementos.forEach(function (elemento) {
+        if (topoPagina > elemento.offsetTop) {
             elemento.classList.add(animacaoClass);
-        }else{
+        } else {
             elemento.classList.remove(animacaoClass)
         }
     });
@@ -36,6 +36,71 @@ function animaScroll(){
         animaScroll();
     })
 } */
-window.addEventListener('scroll',function(){
+window.addEventListener('scroll', function () {
     animaScroll();
 })
+
+
+/* MODAL */
+
+function cliqueModal(img) {
+   /*  const janelaModal = document.getElementById('janelaModal');
+    const imgModal = document.getElementById('imgModal');
+    const txtModal = document.getElementById('txtModal');
+    const btnFechar = document.getElementById('fechar'); */
+
+    /*  janelaModal.classList.remove('escondeJanelaModal');
+     janelaModal.classList.add('mostrarJanelaModal'); */
+}
+
+/* const janelaModal = document.getElementById('janelaModal'); */
+
+
+
+
+
+const janelaModal = document.getElementById('janelaModal');
+const mini = document.querySelectorAll('.mini');
+let imgModal = document.getElementById('imgModal');
+const btnFechar = document.getElementById('fechar');
+const txtModal = document.getElementById('txtModal')
+let srcImg=""
+console.log(mini);
+/* janelaModal.classList.remove('escondeJanelaModal');
+janelaModal.classList.add('mostrarJanelaModal');
+*/
+
+
+for (let i = 0; i < mini.length; i++) {
+    mini[i].addEventListener('click', function () {
+        console.log('oi');
+        srcImg = mini[i].getAttribute('src');
+        imgModal.setAttribute("src",srcImg);
+        txtModal.innerHTML=mini[i].alt;
+        
+        janelaModal.classList.remove('escondeJanelaModal'); 
+       /*  janelaModal.classList.add('mostrarJanelaModal'); */
+        
+        janelaModal.classList.toggle('janela_Ativa');
+        /* imgModal.src.setAttribute('img'); */
+        /* imgModal.src= img.src; */
+
+    });
+}
+
+
+/* imgModal.addEventListener('click',function(){
+   
+}) */
+
+btnFechar.addEventListener('click', function () {
+
+    janelaModal.classList.add('escondeJanelaModal');
+})
+
+/* console.log(mini);  */
+
+/* mini.addEventListener('click',function(){
+
+    mostrarJanela();
+});  */
