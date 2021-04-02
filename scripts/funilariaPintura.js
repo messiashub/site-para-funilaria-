f4157e58f61613a6126eedcbd4705b175738a765
messiashub <claudineimessias@hotmail.com>
2021-04-02 16:38:37 -0300

    update

---
+++ scripts/funilariaPintura.js
@@ -0,0 +1,24 @@
+const nav = document.getElementById('nav1');
+const toNav = nav.offsetTop;
+console.log(nav.classList)
+
+
+window.onscroll = () => {
+    ficarNoTop();
+    /* if(document.body.clientWidth > 480){
+      scrollTopo()  
+    } */
+    
+}
+console.log()
+
+
+function ficarNoTop() {
+    if (window.pageYOffset >= toNav) {
+        nav.classList.add("fixed-top")
+    } else {
+        nav.classList.remove("fixed-top")
+    }
+}
+
+
